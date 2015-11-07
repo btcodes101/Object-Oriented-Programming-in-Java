ec6f494b5b6b5d470acd28b1b2e04397dad9531c
bhaskarcodes <bhaskartejaswi2008@gmail.com>
2015-11-07 20:03:31 +0530

    Add Student Class

---
+++ Week 5/Virtual World/Student.java
@@ -0,0 +1,12 @@
+/**
+ *
+ * @author Bhaskar Tejaswi
+ */
+public class Student extends Person implements move1{
+    
+  public void move(int dist,String dir){
+        System.out.println("STUDENT-walking " + dist+ "km "+dir);
+    }
+   
+    
+}
