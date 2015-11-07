1263d3014fdd5f0f730b8a11c377d23b7c8724f3
bhaskarcodes <bhaskartejaswi2008@gmail.com>
2015-11-07 20:02:51 +0530

    Added CSEStudent Class

---
+++ Week 5/Virtual World/CSEStudent.java
@@ -0,0 +1,14 @@
+/**
+ *
+ * @author Bhaskar Tejaswi
+ */
+public class CSEStudent extends Student implements move1{
+    
+    
+
+    public void move(double dist,String dir)
+    {
+        System.out.println("CSEStudent- running "+ dist+ " km "+dir);
+    }
+   
+}
