6081b721c4706df9886c6703ad65120f053be2df
bhaskarcodes <bhaskartejaswi2008@gmail.com>
2015-11-07 20:03:17 +0530

    Add Person Class

---
+++ Week 5/Virtual World/Person.java
@@ -0,0 +1,13 @@
+/**
+ *
+ * @author Bhaskar Tejaswi
+ */
+public class Person implements move1{
+    
+      public void move(double dist,String dir){
+        System.out.println("PERSON-walking "+ dist+ "km "+dir);
+    }
+    
+    
+    
+}
