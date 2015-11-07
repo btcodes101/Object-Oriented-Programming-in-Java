e6d39b193429f9cd55f5e12560d15ddc29d40782
bhaskarcodes <bhaskartejaswi2008@gmail.com>
2015-11-07 20:03:46 +0530

    Add Vehicle Class

---
+++ Week 5/Virtual World/Vehicle.java
@@ -0,0 +1,10 @@
+/**
+ *
+ * @author Bhaskar Tejaswi
+ */
+public abstract class Vehicle implements move1{
+    
+    public abstract void honk(); 
+    public abstract void move(double a, String b);
+    
+}
